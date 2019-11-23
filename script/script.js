0a838d3915e997cc6bef5be57945f8e46214cae5
Danial Hasan <danialhasan14@gmail.com>
2019-11-23 15:12:13 -0500

    fifth

--- script/script.js
+++ script/script.js
@@ -1,1 +1,16 @@
+var burger = document.getElementById("burger");
+var navContainer = document.getElementById("navBar");
+var count = 0;
+burger.addEventListener("click", function () {
+    if (count == 0) {
+        navContainer.style.display = "block"
+        count = 1
+    } else if (count == 1) {
+        navContainer.style.display = "none"
+        count = 0
+    }
+});
 
+function animateMenuBar(x) {
+    x.classList.toggle("change");
+}
